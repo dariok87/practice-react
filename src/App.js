2041ff228c857c80859b8bd81824f8ea8b5b8cc1
Dario <dkemenovic@gmail.com>
2019-10-01 14:22:50 +0200

    Add event handler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from "react";
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      isLoggedIn: true
-    }
-  }
-  
-  render() {
-    let wordDisplay;
-    if (this.state.isLoggedIn) {
-      wordDisplay = "in"
-    } else {
-      wordDisplay = "out"
-    }
-    return (
-      <div>
-        <h1>You are currently logged {wordDisplay}</h1>
-      </div>
-    )
-  }
+function handClicked() {
+  console.log("i am clicked");
+}
+
+function App() {
+  return (
+    <div>
+      <img src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F72404661%2F219466576930%2F1%2Foriginal.20190911-113241?w=800&auto=compress&rect=0%2C40%2C1280%2C640&s=e348dc04943684a3f80559d108c86a86" />
+      <br />
+      <br />
+      <button onClick={handClicked}>Click me</button>
+    </div>
+  );
 }
 
 export default App;
