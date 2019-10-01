dea6f6d55b19fdd316652d9b6fd36f260a1b6920
Dario <dkemenovic@gmail.com>
2019-10-01 18:49:07 +0200

    Change state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 
-function handClicked() {
-  console.log("i am clicked");
-}
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-function overPic() {
-  console.log("touching picture!")
-}
+  handleClick() {
+    this.setState({ count: 1 })
+  }
 
-function App() {
-  return (
-    <div>
-      <img onMouseOver={overPic} src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F72404661%2F219466576930%2F1%2Foriginal.20190911-113241?w=800&auto=compress&rect=0%2C40%2C1280%2C640&s=e348dc04943684a3f80559d108c86a86" />
-      <br />
-      <br />
-      <button onClick={handClicked}>Click me</button>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    )
+  }
 }
 
 export default App;
