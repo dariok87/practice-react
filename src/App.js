149c13721ac6a888a5e75d9cc8b36e5e7f48e386
Dario <dkemenovic@gmail.com>
2019-10-09 23:58:55 +0200

    Add loading message

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
+      loading: false,
       character: {}
     }
   }
 
   componentDidMount() {
+    this.setState({loading: true})
     fetch("https://swapi.co/api/people/3/")
         .then(response => response.json())
         .then(data => {
           this.setState({
+            loading: false,
             character: data
           })
         })
   }
 
    render() {
+    const text = this.state.loading ? "loading..." : this.state.character.name
     return (
       <div>
-        {this.state.character.name}
+        {text}
       </div>
     )
   }
