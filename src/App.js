f8c85bd3bdac274734a8a5c013e18e84f04539fd
Dario <dkemenovic@gmail.com>
2019-10-13 23:37:45 +0200

    Refactor code

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ class App extends React.Component {
 
   handleChange(event) {
     const {name, value, type, checked} = event.target
-    type === "checkbox" ? this.setState({[name]: checked}) : this.setState({
-      [name]: value
-    })
+    type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
   }
 
    render() {
